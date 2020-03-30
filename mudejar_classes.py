8ed88b72aa34e984a88b01ed60543fb8a655b811
John Marshall <j21marshall@gmail.com>
2020-03-30 14:21:05 -0400

    Multi-lobed arches pt 1

--- mudejar_classes.py
+++ mudejar_classes.py
@@ -1,7 +1,8 @@
 import sys 
 sys.path.append("C:\\Program Files\\FreeCAD 0.18\\bin") 
 import FreeCAD
-import numpy as np
+from numpy import sin, cos, pi
+from numpy.linalg import norm
 
 def cubeVertices(appClass,l,w,h,origin=[0,0]):
     vertices = []
@@ -112,3 +113,38 @@ def lobedArch(appClass, partClass, sketcherClass, name, startN, bottomLeft, heig
     thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+4,2,startN+3,1)) 
     thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+4,1,startN+5,2)) 
     thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+5,1,startN,1))
+    
+def multiLobedArch(appClass, partClass, sketcherClass, name, startN, stepCount, bottomLeft, height, width):
+    thisSketch = eval("appClass.activeDocument()."+name)
+    thisSketch.addGeometry(partClass.LineSegment(appClass.Vector(bottomLeft[0]+width,bottomLeft[1]+height/4,0),appClass.Vector(bottomLeft[0]+width,bottomLeft[1],0)),False)
+    thisSketch.addGeometry(partClass.LineSegment(appClass.Vector(bottomLeft[0]+width,bottomLeft[1],0),appClass.Vector(bottomLeft[0],bottomLeft[1],0)),False)
+    thisSketch.addGeometry(partClass.LineSegment(appClass.Vector(bottomLeft[0],bottomLeft[1],0),appClass.Vector(bottomLeft[0],bottomLeft[1]+height/4,0)),False)
+    thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN,2,startN+1,1)) 
+    thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+1,2,startN+2,1)) 
+    thisSketch.addConstraint(sketcherClass.Constraint('Vertical',startN)) 
+    thisSketch.addConstraint(sketcherClass.Constraint('Vertical',startN+2))
+    largeRadius = width/2*(5/6)
+    step = stepCount
+    stepth = (7*pi/5)/(step-1)
+    centers = []
+
+    for i in range(step):
+        centers.append((bottomLeft[0]+width/2+largeRadius*cos(6*pi/5-i*stepth),bottomLeft[1]+largeRadius*sin(6*pi/5-i*stepth),0))
+    radius = ((centers[1][0]-centers[0][0])**2+(centers[1][1]-centers[0][1])**2)**(1/2)/2
+
+    thisSketch.addGeometry(partClass.ArcOfCircle(partClass.Circle(appClass.Vector(centers[0][0],bottomLeft[1]+height/4,0),appClass.Vector(0,0,1),centers[0][0]-bottomLeft[0]),pi/2,pi),False)
+    thisSketch.addGeometry(partClass.ArcOfCircle(partClass.Circle(appClass.Vector(centers[-1][0],bottomLeft[1]+height/4,0),appClass.Vector(0,0,1),centers[0][0]-bottomLeft[0]),0,pi/2),False)
+    thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+3,2,startN+2,2)) 
+    thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+4,1,startN,1)) 
+
+    thisSketch.addGeometry(partClass.ArcOfCircle(partClass.Circle(appClass.Vector(centers[0][0],height/4+largeRadius+radius+centers[0][1],0),appClass.Vector(0,0,1),radius),17*pi/10-pi,3*pi/2),False)
+    for i in range(1,len(centers)-1):
+        thisSketch.addGeometry(partClass.ArcOfCircle(partClass.Circle(appClass.Vector(centers[i][0],height/4+largeRadius+radius+centers[i][1],0),appClass.Vector(0,0,1),radius),17*pi/10-i*stepth-pi,17*pi/10-i*stepth),False)
+    thisSketch.addGeometry(partClass.ArcOfCircle(partClass.Circle(appClass.Vector(centers[-1][0],height/4+largeRadius+radius+centers[-1][1],0),appClass.Vector(0,0,1),radius),-pi/2,17*pi/10-(len(centers)-1)*stepth),False)
+    for i in range(5,len(centers)+4):
+        thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+i+1,2,startN+i,1))
+    thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+5,2,startN+3,1)) 
+    thisSketch.addConstraint(sketcherClass.Constraint('Coincident',startN+step+4,1,startN+4,2))
+
+    thisSketch.movePoint(startN+4,3,appClass.Vector(centers[-1][0],bottomLeft[1]+height/4,0),0)
+    return startN+step+5
